e0bda4e0ba0bd2e481263936399f8302fb74eba2
Franklin Ma <105677497+terwo@users.noreply.github.com>
2024-03-05 17:37:25 -0800

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,3 @@
-// The popup shows up directly to the right of the highlighted text
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//     document.querySelector('.popup-content').innerText = request.selectedText;
-// });
-
 var newheight = window.height * (5 / 10);
 var newwidth = window.width * (7 / 10);
 window.resizeTo(newheight, newwidth);
